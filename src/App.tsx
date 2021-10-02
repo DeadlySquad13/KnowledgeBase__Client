62d5398c4455306875f5bea9e964d7f0cac27c66
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2021-10-03 02:45:09 +0300

    Post and Get tested

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { gql, useQuery } from '@apollo/client';
 
-function App() {
+const getUser = async () => {
+  const response = await fetch('http://localhost:6013/', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/text',
+    }
+  });
+
+  if (response.status !== 200) {
+    console.error('getUser error!');
+  }
+
+  return response;
+}
+
+const createUser = async (name: string) => {
+  const response = await fetch('http://localhost:6013/', {
+    method: 'POST',
+    body: JSON.stringify({
+      name: name
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
+
+  if (response.status !== 201) {
+    console.error('createUser error!');
+  }
+
+  return response;
+}
+
+const getUserQuery = gql`
+  query user {
+    getUser
+  }
+`;
+
+const App = () => {
+  const [user, setUser] = useState('');
+
+  const response = useQuery<{ getUser: string } >(getUserQuery);
+
+  useEffect(() => {
+    createUser('Vanya')
+      .then(async (res) => await res.text())
+      .then((data) => console.log(data))
+      .catch((errorReason) => console.error(errorReason));
+
+    getUser()
+      .then(async (res) => await res.text())
+      .then((data) => setUser(data))
+      .catch((errorReason) => console.error(errorReason));
+
+  }, []);
+
+  useEffect(() => {
+    console.log(response.data?.getUser);
+  }, [response])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +77,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          { user }
         </a>
       </header>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:6013/graphql',
+    cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
