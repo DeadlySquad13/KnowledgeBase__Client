8d5ac66c75ff6c5ec5c6b4dacbd29788d0ef2900
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2021-10-03 16:59:47 +0300

    KB__C-2-user (#2)
    User will be changed to wholesome object, simple PATCH method will be changed to POST that's why corresponding function is named 'createUser', not 'modifyUser'.

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useEffect, useState } from 'react';
+import logo from './DirtySasha.png';
 import './App.css';
-import { gql, useQuery } from '@apollo/client';
-
-const getUser = async () => {
-  const response = await fetch('http://localhost:6013/', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/text',
-    }
-  });
-
-  if (response.status !== 200) {
-    console.error('getUser error!');
-  }
-
-  return response;
-}
-
-const createUser = async (name: string) => {
-  const response = await fetch('http://localhost:6013/', {
-    method: 'POST',
-    body: JSON.stringify({
-      name: name
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    }
-  });
-
-  if (response.status !== 201) {
-    console.error('createUser error!');
-  }
-
-  return response;
-}
+import { gql, useMutation, useQuery } from '@apollo/client';
+import styled from 'styled-components';
+import { margin, MarginProps } from 'styled-system';
 
 const getUserQuery = gql`
   query user {
@@ -42,43 +11,86 @@ const getUserQuery = gql`
   }
 `;
 
+const createUserMutation = gql`
+  mutation user($input: String!) {
+    createUser(name: $input)
+  }
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Label = styled.label<MarginProps>`
+  ${margin}
+`;
+
+const Input = styled.input<MarginProps>`
+  ${margin}
+  padding: 4px 10px;
+
+  width: 200px;
+  height: 30px;
+
+  border: 1px  solid black;
+  border-radius: 6px;
+`;
+
+const Button = styled.button<MarginProps>`
+  ${margin}
+  padding: 4px 10px;
+
+  width: 200px;
+  height: 30px;
+
+  border: 1px solid black;
+  border-radius: 6px;
+
+  &:hover {
+    background: #61DAFB;
+  }
+`;
+
 const App = () => {
   const [user, setUser] = useState('');
+  const [name, setName] = useState('');
 
-  const response = useQuery<{ getUser: string } >(getUserQuery);
+  const [newUser] = useMutation<{ createUser: string }>(createUserMutation, { refetchQueries: [getUserQuery] });
+  const { data: getUserData } = useQuery<{ getUser: string }>(getUserQuery);
+
+  const onCreateButtonClick = useCallback(() => {
+    console.log('input value in button:', name);
+    newUser({
+      variables: {
+        input: name
+      }
+    });
+  }, [newUser, name]);
+
+  const handleInputchange = useCallback((e) => setName(e.target.value), []);
 
   useEffect(() => {
-    createUser('Vanya')
-      .then(async (res) => await res.text())
-      .then((data) => console.log(data))
-      .catch((errorReason) => console.error(errorReason));
+    setUser(getUserData?.getUser || '');
+  }, [getUserData?.getUser])
 
-    getUser()
-      .then(async (res) => await res.text())
-      .then((data) => setUser(data))
-      .catch((errorReason) => console.error(errorReason));
-
-  }, []);
-
-  useEffect(() => {
-    console.log(response.data?.getUser);
-  }, [response])
+  const handleFormSubmit = useCallback((e) => e.preventDefault(), []);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          { user }
-        </a>
+        <h2>Кто сегодня мужчина судьбы?</h2>
+        <Form onSubmit={handleFormSubmit}>
+          <Label>Вот этот молодой человек:</Label>
+          <Input mt="10px" type="text" value={name} onChange={handleInputchange}/>
+          <Button mt="10px" onClick={onCreateButtonClick}>Да, да, этот</Button>
+        </Form>
+        <h3 className="App-link">
+          { user || '...' } свеж молод светел!
+        </h3>
       </header>
     </div>
   );
